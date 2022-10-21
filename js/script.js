02e952afc648fcbbe95dc30003c7b7022d2eaeb0
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-10-21 13:53:27 +0200

    [add] bombGen

--- js/script.js
+++ js/script.js
@@ -1,22 +1,39 @@
-const btn = document.getElementById('play')
+/*Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati.
+In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.*/
+
+
+//1.generare 16 bombe univoche
+//2.se clicco cella ed idcella = bomba -> perso (colore rosso) ELSE 
+// si colora di azzurro idcella != bomba continuo a giocare
+//3.END= se prendo una bomba o se sseleziono tutte le caselle giuste
+//4.tengo conto del punteggio e lo comunico alla fine
+//5.resetto e rincolmincio
+
+const btnPlay = document.getElementById('play')
 const container = document.querySelector('.container');
 let squareForRow = 0;
+const NUMBER_BOMS = 3;
+let bomb = [];
 
-btn.addEventListener('click', function(){
-  squareForRow = document.getElementById('level').value;
-  container.innerHTML = ''
-  init()
-});
-
+btnPlay.addEventListener('click', play);
 
 /**
  * Funzione di inizializazione
  */
-function init() {
+function play() {
+  //prendo input livello di difficoltà
+  squareForRow = document.getElementById('level').value;
+  container.innerHTML = ''
   //salvo il numero dei quadratini
   const totalSquare = squareTot(squareForRow);
   //salvo il numero totale dei quadratini
   squareGen(totalSquare);
+
+  bomb = bombGen(NUMBER_BOMS);
+  console.log(bomb);
 }
 
 /**
@@ -40,11 +57,24 @@ function squareGen(nSquare){
     square.style.width = `calc(100% / ${squareForRow}`
     square.style.height = `calc(100% / ${squareForRow}`
     container.append(square);
-    square.idSq = i + 1;
+    square.innerText = i + 1;
     square.addEventListener('click', function(){
-      square.innerText = i + 1;
+      square.idSq = i + 1;
       square.classList.add('color')
       console.log(this.idSq);
     })
   }
+}
+
+function bombGen(NUMBER_BOMS){
+  let bombsArray = [];
+  do{
+    let bombs = Math.floor(Math.random() * NUMBER_BOMS) + 1;
+    if(!bombsArray.includes(bombs)){
+      bombsArray.push(bombs);
+    }
+  }
+  while (bombsArray.length < NUMBER_BOMS);
+
+  return bombsArray;
 }
