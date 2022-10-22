0e356ad2359426a39533bc027cb272d1e6972127
RiccardoLoPresti <riccardolopresti35@gmail.com>
2022-10-22 15:27:31 +0200

    [fix] gameover and win

--- js/script.js
+++ js/script.js
@@ -1,102 +1,102 @@
-/*Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
-I numeri nella lista delle bombe non possono essere duplicati.
-In seguito l’utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, altrimenti la cella cliccata si colora di azzurro e l’utente può continuare a cliccare sulle altre celle.
-La partita termina quando il giocatore clicca su una bomba o raggiunge il numero massimo possibile di numeri consentiti.
-Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.*/
-
-
-//1.generare 16 bombe univoche
-//2.se clicco cella ed idcella = bomba -> perso (colore rosso) ELSE 
-// si colora di azzurro idcella != bomba continuo a giocare
-//3.END= se prendo una bomba o se sseleziono tutte le caselle giuste
-//4.tengo conto del punteggio e lo comunico alla fine
-//5.resetto e rincolmincio
-
 const btnPlay = document.getElementById('play')
 const container = document.querySelector('.container');
-let squareForRow = 0;
-const NUMBER_BOMS = 3;
-let bomb = [];
+let cellePerRiga = 0;
+let celleTotali = 0;
+const numeroBombeTotali = 16;
+let bombe = [];
 let score = 0;
-
+let array =[];
 
 btnPlay.addEventListener('click', play);
 
-/**
- * Funzione di inizializazione
- */
-function play() {
-  //prendo input livello di difficoltà
-  squareForRow = document.getElementById('level').value;
-  container.innerHTML = ''
-  //salvo il numero dei quadratini
-  const totalSquare = squareTot(squareForRow);
-  //salvo il numero totale dei quadratini
-  squareGen(totalSquare);
+function play(){
+  score = 0
+  array =[]
+  container.innerHTML ='';
+  cellePerRiga = document.getElementById('level').value;
+  celleTotali = calcoloGriglia(cellePerRiga);
+  bombe = generatoreBombe(numeroBombeTotali,celleTotali)
+  generatoreCelle(celleTotali);
 
-  bomb = bombGen(NUMBER_BOMS,totalSquare);
-  console.log(bomb);
-  console.log(bomb);
+  console.log(bombe);
 }
 
 
-/**
- * Calcola il numero massimo dei quadratini
- * @param {number} numberSquareForRow 
- * @returns totale Square
- */
-function squareTot(numberSquareForRow){
-  return totSquare = Math.pow(numberSquareForRow, 2);
+function calcoloGriglia(cellePerRiga) {
+  return celleTotali = Math.pow(cellePerRiga, 2);
 }
 
-/**
- * Genera il numero di quadrati necesari e lo stampa in HMTL facendo cambiare colore al click della casella
- * @param {number} nSquare 
- * @returns square stampati in html
- */
-function squareGen(nSquare){
-  for(let i = 0; i < nSquare; i++){
-    let square = document.createElement('div');
-    square.className = "square";
-    square.style.width = `calc(100% / ${squareForRow}`
-    square.style.height = `calc(100% / ${squareForRow}`
-    container.append(square);
-    square.innerText = i + 1;
-
-    square.addEventListener('click', function(){
-      
-      square.idSq = i + 1;
-      const elementId = square.idSq
-
-      if(bomb.includes(elementId)){
-        square.classList.add('bomb');
-        const output = document.createElement('div');
-        output.innerHTML = `<span>HAI PERSO! Hai fatto 
-        ${score-1} punti su xxx</span>`;
-        container.append(output)
-      }else{
-        //aumento score
-        score++
-        console.log('score', score);
-        square.classList.add('color')
-        console.log(this.idSq);
-        if (score === (totalSquare - NUMBER_BOMS)) {
-          console.log('vinto');
-        }
-      }
-    })
-  }
-}
-
-function bombGen(NUMBER_BOMS,totalSquare){
-  let bombsArray = [];
+function generatoreBombe(numeroBombeTotali,celleTotali){
+  let arrayBombe = [];
   do{
-    let bombs = Math.floor(Math.random() * totalSquare) + 1;
-    if(!bombsArray.includes(bombs)){
-      bombsArray.push(bombs);
+    let nbomba = Math.floor(Math.random() * celleTotali) + 1;
+    if(!arrayBombe.includes(nbomba)){
+      arrayBombe.push(nbomba);
     }
   }
-  while (bombsArray.length < NUMBER_BOMS);
+  while (arrayBombe.length < numeroBombeTotali);
 
-  return bombsArray;
+  return arrayBombe;
+}
+
+function generatoreCelle(celleTotali){
+  for(let i = 0; i < celleTotali; i++){
+    let cella = document.createElement('div');
+    cella.className = "square";
+    dimensione(cella)
+    container.append(cella);
+    cella.innerText = i + 1;
+    clickEvent(cella,i);
+  }
+}
+
+function dimensione(cella) {
+  cella.style.width = `calc(100% / ${cellePerRiga}`
+  cella.style.height = `calc(100% / ${cellePerRiga}`
+}
+
+function clickEvent(cella, i) {
+    cella.addEventListener('click', function(){
+      this.idCella = i + 1;
+      const idCella = this.idCella;
+      const cella = document.getElementsByClassName('square')
+
+      if(!array.includes(idCella) && !bombe.includes(idCella)){
+        score++;
+        cella[i].classList.add('color');
+        array.push(idCella);
+        let vincita = celleTotali - numeroBombeTotali;
+        if (score == vincita) {
+          win(vincita)
+        }
+      }
+      if(bombe.includes(idCella)){
+        cella[i].classList.add('bomb')
+        gameOver();
+      }
+    })
+}
+
+function gameOver() {
+  const output = document.createElement('div');
+  const layover = document.createElement('div');
+  output.className = 'output'
+  output.innerHTML = `
+    <span>HAI PERSO! Hai fatto ${score} punti su ${celleTotali - numeroBombeTotali}</span>
+  `;
+  container.append(output);
+  layover.className = 'layover';
+  container.prepend(layover);
+}
+
+function win(){
+  const output = document.createElement('div');
+  const layover = document.createElement('div');
+  output.className = 'output'
+  output.innerHTML = `
+    <span>HAI VINTO!!</span>
+  `;
+  container.append(output)
+  layover.className = 'layover';
+  container.prepend(layover);
 }
